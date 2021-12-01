60fb5d86f8ca1b01ea61dde487821de67df50e5b
Furkan Altan <furkanaltan1@gmail.com>
2021-12-01 16:00:30 +0300

    Ödev 2

---
+++ src/app.js
@@ -0,0 +1,23 @@
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 bu olmaz 
+console.log(euroDun)
+
+//let users = getUsersFromApi()
+
+let konutKredileri = [ "Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Furkan Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+   
+}
+//console.log('</ul>')
